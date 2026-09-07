644bbb7982dccc34be7453846125c5a77f6325f3
cpm777787bomba-arch <cpm777787bomba@gmail.com>
2026-09-07 15:34:10 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace F_Math
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "F-Math" };
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace F_Math
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
